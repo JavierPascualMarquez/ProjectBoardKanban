f0e60b9905edac7d989d8f57d907a68345fd7ebb
JavierPascualMarquez <106414318+JavierPascualMarquez@users.noreply.github.com>
2022-08-13 21:58:26 -0300

    PYTHON_EJERCICIO_7.py
    Ejercicio numero 7 de la Valoración final del modulo de Introducción a la programación del ISPC

---
+++ PYTHON_EJERCICIO_7.py
@@ -0,0 +1,9 @@
+Historial3=(9530, 4120, 4580, 1500, 890, 7516 ,426)
+
+Lennon = sum(Historial3)/len(Historial3)
+
+if Lennon > 4500:
+    print ("Se ha excedido del gasto promedio para su mascota")
+    
+else:
+    print ("No se ha excedido el gasto promedio para su mascota")
